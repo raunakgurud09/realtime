feb36092f241c503a0d49cd448e9c06a7e6b1b11
Raunak Gurud <raunakgurud2121@gmail.com>
2023-12-02 11:54:40 +0530

    chore: app setup and  connection with database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('changes')
+import dotenv from "dotenv";
+import { httpServer } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const majorNodeVersion = +process.env.NODE_VERSION?.split(".")[0] || 0;
+
+const startServer = () => {
+  httpServer.listen(process.env.PORT || 8080, () => {
+    console.info(
+      `📑 Visit the documentation at: http://localhost:${
+        process.env.PORT || 8080
+      }`
+    );
+    console.log("⚙️  Server is running on port: " + process.env.PORT);
+  });
+};
+
+if (majorNodeVersion >= 14) {
+  try {
+    await connectDB();
+    startServer();
+  } catch (err) {
+    console.log("Mongo db connect error: ", err);
+  }
+} else {
+  connectDB()
+    .then(() => {
+      startServer();
+    })
+    .catch((err) => {
+      console.log("Mongo db connect error: ", err);
+    });
+}
