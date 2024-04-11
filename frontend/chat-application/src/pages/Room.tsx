775554a58f747d02e0730491b0086ede1b102525
Raunak Gurud <raunakgurud2121@gmail.com>
2024-04-11 09:19:56 +0530

    refactor: room page code

--- frontend/chat-application/src/pages/Room.tsx
+++ frontend/chat-application/src/pages/Room.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState } from "react";
 import ReactPlayer from "react-player";
 import peer from "../service/peer";
 import { useSocket } from "../context/SocketContext";
